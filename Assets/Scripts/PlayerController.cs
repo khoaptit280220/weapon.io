1866b3a53a8a0df4d6dae7549c0886f0a87ec1d4
Khoant <khoant.cntt.ptit2802@gmail.com>
2022-12-24 00:11:53 +0700

    add new joystick

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,17 @@ public class PlayerController : MonoBehaviour
     private float yRangeDown = -75;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Vector3 vectorDirectionMove;
+    private float inputX = 0;
+    private float inputY = 0;
+    private bool firstJoystick;
+
+    public DynamicJoystick dynamicJoystick;
+
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
+        firstJoystick = false;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -24,15 +32,31 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+        if (dynamicJoystick.Horizontal != 0 || dynamicJoystick.Vertical != 0)
+        {
+         
+            direction = new Vector3(dynamicJoystick.Horizontal, 0.0f, dynamicJoystick.Vertical);
+            Vector3 _rotation = new Vector3(0,0
+                , -Mathf.Atan2(dynamicJoystick.Horizontal, dynamicJoystick.Vertical) * Mathf.Rad2Deg);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(_rotation),
+                speedRotate * Time.deltaTime);
+
+          //  transform.position += direction * speed * Time.deltaTime;
+        }
     }
 
-    private void FixedUpdate()
-    {
-        float inputX = CnInputManager.GetAxis("Horizontal");         
-        float inputY = CnInputManager.GetAxis("Vertical");         
-        vectorDirectionMove = new Vector3(inputX, inputY, 0f);
-        rb.velocity = vectorDirectionMove.normalized * speed;
-    }
+    // private void FixedUpdate()
+    // {
+    //     float inputX = CnInputManager.GetAxis("Horizontal");         
+    //     float inputY = CnInputManager.GetAxis("Vertical");    
+    //     Vector3 _rotation = new Vector3(0,0
+    //         , -Mathf.Atan2(inputX, inputY) * Mathf.Rad2Deg);
+    //   
+    //     transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(_rotation),
+    //         speedRotate * Time.deltaTime);
+    //   //  rb.velocity = vectorDirectionMove.normalized * speed;
+    // }
 
     /*public void OnCollisionEnter(Collision collision)
     {
